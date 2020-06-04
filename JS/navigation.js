7c90d5c76b11ae6e406df3ac50d8a008f43fe5c9
p4ndor1c4 <53515244+p4ndor1c4@users.noreply.github.com>
2020-06-04 18:28:12 +0200

    title as home button

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,17 +1,16 @@
 const navData = [
-    {title: 'Home', link: '/WSOA3028A_1828768/'},
-    {title: 'About Me', link: '/WSOA3028A_1828768/About'},
-    {title: 'Blog', link: '/WSOA3028A_1828768/Blog'},
-    {title: 'Bucket List', link: '/WSOA3028A_1828768/BucketList'}
+    { title: 'Blog', link: '/WSOA3028A_1828768/Blog' },
+    { title: 'About Me', link: '/WSOA3028A_1828768/About' },
+    { title: 'Resources', link: '/WSOA3028A_1828768/Resources' },
+    { title: 'Bucket List', link: '/WSOA3028A_1828768/BucketList' }
 ];
 
-function initialiseNavigation(){
+function initialiseNavigation() {
     const nav = document.querySelector('nav');
 
     const ul = document.createElement('ul');
 
-    for (let i = 0; i < navData.length; i++)
-    {
+    for (let i = 0; i < navData.length; i++) {
         const li = document.createElement('li');
         const a = document.createElement('a');
         a.innerText = navData[i].title;
