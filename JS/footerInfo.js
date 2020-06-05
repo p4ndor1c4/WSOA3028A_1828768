b0f2c71cd8ab55f3975dd41bbff17a12623a3d8c
p4ndor1c4 <53515244+p4ndor1c4@users.noreply.github.com>
2020-06-05 16:16:34 +0200

    a whole lot of stuff

---
+++ JS/footerInfo.js
@@ -0,0 +1,32 @@
+const icons = [
+    { source: '/WSOA3028A_1828768/Images/instagram-logo.png', link: 'https://www.instagram.com/p4ndor1c4/' },
+    { source: '/WSOA3028A_1828768/Images/twitter.png', link: 'https://twitter.com/p4ndor1c4' }
+];
+
+const hCardData = [
+    '<p class="p-name">Grace Durden</p>',
+    '<a class="u-email" href="mailto:grace.d.wanderlust@gmail.com">grace.d.wanderlust@gmail.com</a>',
+    '<p class="p-locality">Johannesburg</p>',
+    '<p class="p-country-name">South Africa</p>'
+];
+
+function generateFooter() {
+    const footer = document.querySelector('footer');
+
+    for (let i = 0; i < icons.length; i++) {
+        const img = document.createElement('img');
+        img.src = icons[i].source;
+        const a = document.createElement('a');
+        a.href = icons[i].link;
+        a.appendChild(img);
+        footer.appendChild(a);
+    }
+
+    for (let i = 0; i < hCardData.length; i++) {
+        footer.innerHTML += hCardData[i];
+    }
+
+    const div = document.createElement('div');
+    div.innerHTML = 'Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>';
+    footer.appendChild(div);
+}
